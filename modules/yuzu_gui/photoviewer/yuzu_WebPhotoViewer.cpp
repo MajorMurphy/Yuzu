e8cfda13a489204a2aa5a5feb1bf3c31e4d6acc4
MajorMurphy <major@murphyindustries.net>
2024-07-30 14:44:32 -0400

    converting to compatible image format for webkit

--- modules/yuzu_gui/photoviewer/yuzu_WebPhotoViewer.cpp
+++ modules/yuzu_gui/photoviewer/yuzu_WebPhotoViewer.cpp
@@ -4,13 +4,14 @@
 #if JUCE_WEB_BROWSER
 using namespace juce;
 
-yuzu::WebPhotoViewer::WebPhotoViewer() : 
+yuzu::WebPhotoViewer::WebPhotoViewer() :
 	juce::WebBrowserComponent(Options()
 		.withBackend(Options::Backend::webview2)
 		.withWinWebView2Options(WebBrowserComponent::Options::WinWebView2{}
 			.withUserDataFolder(yuzu::FileHelpers::getTempFolder())))
 {
 	setOpaque(true);
+	disableContextMenu();
 }
 
 yuzu::WebPhotoViewer::~WebPhotoViewer()
@@ -22,11 +23,52 @@ void yuzu::WebPhotoViewer::loadImage(juce::File file)
 	if (!file.existsAsFile())
 		return;
 
-	if (!yuzu::JPEGImageExtendedFormat::usesFileExtension(file))
+	if (!yuzu::JPEGImageExtendedFormat::usesFileExtension(file) &&
+		!yuzu::PNGImageExtendedFormat::usesFileExtension(file) &&
+		!yuzu::GIFImageExtendedFormat::usesFileExtension(file) &&
+		!yuzu::WEBPImageExtendedFormat::usesFileExtension(file))
+	{
+		auto image = ExtendedImageFileFormat::findImageFormatForFile(file);
+		if (image)
+			loadImage(image->decodeImage());
+		else jassert(false);
+
 		return;
+	}
 
 	URL url(file);
 	goToURL(url.toString(true));
+	disableContextMenu();
+	
+}
+
+void yuzu::WebPhotoViewer::loadImage(juce::Image image)
+{
+	if (image.isNull())
+	{
+		return;
+	}
+
+	juce::JPEGImageFormat jpg;
+	jpg.setQuality(0.9);
+	auto file = FileHelpers::getTempFolder().getChildFile("tempimg.jpg");
+	file.deleteFile();
+	auto os = file.createOutputStream();
+	if (os)
+	{
+		jpg.writeImageToStream(image, *os);
+		os->flush();
+	}
+	else jassert(false);
+
+	URL url(file);
+	goToURL(url.toString(true));
+	disableContextMenu();
+}
+
+void yuzu::WebPhotoViewer::disableContextMenu()
+{
+	evaluateJavascript("document.body.style.backgroundColor = \"#00000000\";");
 	evaluateJavascript("document.addEventListener('contextmenu', event => event.preventDefault());");
 }
 #endif
--- modules/yuzu_gui/photoviewer/yuzu_WebPhotoViewer.h
+++ modules/yuzu_gui/photoviewer/yuzu_WebPhotoViewer.h
@@ -16,9 +16,10 @@ namespace yuzu
         ~WebPhotoViewer() override;
 
         void loadImage(juce::File imageFile);
+        void loadImage(juce::Image img);
 
     private:
-
+        void disableContextMenu();
 
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WebPhotoViewer)
     };
